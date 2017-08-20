e328dbd1e41823cda473be00011b36e4b4225f44
Hana <hanna.101@hotmail.com>
2017-08-20 18:51:12 +0300

    mention function

--- bot.py
+++ bot.py
@@ -3,9 +3,12 @@ import twitter
 import codecs
 import sys
 
-
+# we read our keys to connect to twitter api
 api = twitter.Api(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, 
                 access_token_key=ACCESS_TOKEN, access_token_secret=ACCESS_SECRET)
-statuses = api.GetStatus(898425485608210432)
 
-print(statuses)
+# we use method called GetStatus to get detials about a specific status writter in twitter
+statuses = api.GetStatus(898442237381455873)
+mentions_list= api.GetMentions()
+#mentions_list[0] = '1'
+print(len(mentions_list))
