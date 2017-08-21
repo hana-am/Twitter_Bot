a0021d5a259e32c5bf5cae9ca2cde9d15d83617b
Hana <hanna.101@hotmail.com>
2017-08-21 20:27:00 +0300

    fetching replies of the tweet owner

--- bot.py
+++ bot.py
@@ -2,13 +2,28 @@ from secrets import *
 import twitter
 import codecs
 import sys
-
+from TwitterAPI import TwitterAPI
 # we read our keys to connect to twitter api
-api = twitter.Api(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, 
+api = TwitterAPI(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET, 
                 access_token_key=ACCESS_TOKEN, access_token_secret=ACCESS_SECRET)
-
-# we use method called GetStatus to get detials about a specific status writter in twitter
-statuses = api.GetStatus(898442237381455873)
-mentions_list= api.GetMentions()
-#mentions_list[0] = '1'
-print(len(mentions_list))
+"""
+r = api.request('statuses/show/:%d' % 898442237381455873)
+for item in r.get_iterator():
+    print(item['text'])
+"""
+'''
+print("============================")
+username= 'ThisArchive'
+original_tweet= 898442237381455873
+r = api.request('search/tweets', {'q':'to:%s' % username})
+for k in r.get_iterator():
+    if k['in_reply_to_status_id'] == original_tweet:
+        print(k['text'])
+'''
+username= 'ZainB'
+original_tweet= 898185380981288960
+r = api.request('search/tweets', {'q':'to:%s' % username})
+for k in r.get_iterator():
+    if k['in_reply_to_status_id'] == original_tweet :
+        print(k['text'])
+        #print(k['user']['screen_name'])
